ac1c3a1e6b2ca29d002215d2346919a5d8d28089
bowen0701 <bowen0701@gmail.com>
2020-04-12 09:04:52 +0200

    Revise board to numeric vals

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -11,17 +11,18 @@ NMARKS = 3
 BOARD_NROWS = BOARD_NCOLS = 3
 BOARD_SIZE = BOARD_NROWS * BOARD_NCOLS
 
+CROSS = 1
+CIRCLE = -1
+EMPTY = 0
+
 
 def hash(board):
-    return ''.join(list(board.reshape(BOARD_SIZE)))
-
-def unhash(state):
-    return np.array(list(state)).reshape((BOARD_NROWS, BOARD_NCOLS))
+    return ','.join([str(x) for x in list(board.reshape(BOARD_SIZE))])
 
 
 class State:
     def __init__(self):
-        self.board = np.array([' '] * BOARD_SIZE).reshape((BOARD_NROWS, BOARD_NCOLS))
+        self.board = np.array([EMPTY] * BOARD_SIZE).reshape((BOARD_NROWS, BOARD_NCOLS))
         self.state = hash(self.board)
         self.winner = None
         self.is_end = False
@@ -30,37 +31,41 @@ class State:
         """Judge winner and is_end based on the current state."""
         # Iterate each row to judge winner.
         for r in range(BOARD_NROWS):
-            row = ''.join(self.board[r])
+            row = self.board[r, :]
             symbol = row[0]
-            if symbol != ' ' and row.count(symbol) == NMARKS:
+            if symbol != EMPTY and np.sum(row) == symbol * NMARKS:
                 self.winner = symbol
                 self.is_end = True
+                return self
         
         # Iterate each col to judge winner.
         for c in range(BOARD_NCOLS):
-            col = ''.join(self.board[:][c])
+            col = self.board[:, c]
             symbol = col[0]
-            if symbol != ' ' and col.count(symbol) == NMARKS:
+            if symbol != EMPTY and np.sum(col) == symbol * NMARKS:
                 self.winner = symbol
                 self.is_end = True
+                return self
         
         # Check 2 diagonals to judge winner.
         symbol = self.board[1][1]
-        if symbol != ' ': 
+        if symbol != EMPTY: 
             diag1, diag2 = [], []
             for i in range(BOARD_NROWS):
                 diag1.append(self.board[i][i])
                 diag2.append(self.board[i][BOARD_NROWS - i - 1])
 
-            diag1, diag2 = ''.join(diag1), ''.join(diag2)
-            if diag1.count(symbol) == NMARKS or diag2.count(symbol) == NMARKS:
+            diag1, diag2 = np.array(diag1), np.array(diag1)
+            if np.sum(diag1) == symbol * NMARKS or np.sum(diag2) == symbol:
                 self.winner = symbol
                 self.is_end = True
+                return self
 
         # Judge tie with no winner.
-        if self.state.count('X') + self.state.count('O') == BOARD_SIZE:
+        if np.sum(np.abs(self.board)) == BOARD_SIZE:
             self.is_end = True
-    
+            return self
+
     def next_state(self, r, c, symbol):
         """Create the next state at position (r, c)."""
         next_state = State()
@@ -69,22 +74,6 @@ class State:
         next_state.state = hash(next_state.board)
         next_state.judge()
         return next_state
-
-    def _dfs_all_states_recur(self, symbol):
-        states_d = dict()
-        for r in range(BOARD_NROWS):
-            for c in range(BOARD_NCOLS):
-                if self.board[r][c] == ' ':
-                    next_state = self.next_state(r, c, symbol)
-    
-    def dfs_all_states(self, symbol):
-        states_d = dict()
-        for r in range(BOARD_NROWS):
-            for c in range(BOARD_NCOLS):
-                if self.board[r][c] == ' ':
-                    next_state = self.next_state(r, c, symbol)
-                    if next_state.state not in states_d:
-                        pass
 
 
 def _dfs_states_recur(cur_symbol, cur_state, states_d):
