1de0755394b6b1c72f9e39454133872d77912675
bowen0701 <bowen0701@gmail.com>
2020-08-23 22:32:25 +0800

    Remove redundant itertools import

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -2,11 +2,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import itertools
-
-import numpy as np
 import json
 import re
+
+import numpy as np
 
 
 NMARKS = 3
