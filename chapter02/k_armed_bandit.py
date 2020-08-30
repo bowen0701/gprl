2c33cf37b85afbc971b1052d2d4eb8cc9b0d1f17
bowen0701 <bowen0701@gmail.com>
2020-08-30 19:42:07 +0800

    Impl environment & agent classes

--- chapter02/k_armed_bandit.py
+++ chapter02/k_armed_bandit.py
@@ -5,11 +5,22 @@ from __future__ import print_function
 import numpy as np
 
 
+
+class Environment::
+	"""Environment class for k-armed bandit."""
+
+	def __init__(self, K):
+		# Simulate K means from standard normal N(0, 1).
+		self.means = np.random.normal(0, 1, K)
+
+
 class MultiArmedBanditAgent:
 	"""Agent class for k-armed bandit."""
 
-	def __init__(self, k=10):
-		pass
+	def __init__(self, K):
+		# Init K action-values Q(A) and counts N(A) for action A.
+		self.Q = [0] * K
+		self.N = [0] * K
 
 
 def k_armed_testbed():
